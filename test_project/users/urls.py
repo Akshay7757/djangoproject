1919c7686e4cd76f8c81b41b632e5061be01998d
Akshay7757 <akshayp.jadhav@gmail.com>
2023-12-13 17:04:52 +0530

    whole project updated

--- test_project/users/urls.py
+++ test_project/users/urls.py
@@ -5,9 +5,43 @@ urlpatterns = [
 
 
     path('', views.index, name='index'),
-    
+    # contact 
     path('contact', views.contact, name='contact'),
+     # Python 
+    path('python', views.python, name='python'),
+    # FreshersJob 
+    path('freshersjob', views.freshersjob, name='freshersjob'),
+    # Experience Job 
+    path('experience', views.experience, name='experience'),
+    # Freshers Placement 
+    path('Fplacement', views.Fplacement, name='Fplacement'),
+    # Experience Placement
+    path('Eplacement', views.Eplacement, name='Eplacement'),
+    # Testimonial
+    path('Testimonial', views.Testimonial, name='Testimonial'),
+    # Drives Cyber Success
+    path('DCS', views.DCS, name='DCS'),
+    # Careers
+    path('Careers', views.Careers, name='Careers'),
+    # Clients
+    path('Clients', views.Clients, name='Clients'),
+    # Blogs
+    path('Blogs', views.Blogs, name='Blogs'),
 
-    path('python', views.python, name='python')
+   
+
+    
+    
+    
+
+
+
+    
+    
+
+    
+
+    
+
 
 ]
--- test_project/users/views.py
+++ test_project/users/views.py
@@ -14,6 +14,7 @@ def index(request):
 
     return HttpResponse(template.render(context))
 
+#  Python Course
 def python(request):
     template = loader.get_template("python.html")
     
@@ -25,7 +26,117 @@ def python(request):
 
     return HttpResponse(template.render(context))
 
+#  Fresher Jobs
+def freshersjob(request):
+    template = loader.get_template("news-single.html")
+    
+    user_list = User.objects.all().values()
 
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+#  Experience Jobs
+def experience(request):
+    template = loader.get_template("experence.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+#Fresher Placement
+def Fplacement(request):
+    template = loader.get_template("fplace.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))   
+
+#Experience Placement
+def Eplacement(request):
+    template = loader.get_template("eplace.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+#Testimonials  
+                    
+def Testimonial(request):
+    template = loader.get_template("tens.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+#Driver Cyber Success
+def DCS(request):
+    template = loader.get_template("campus.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+# Careers       
+def Careers(request):
+    template = loader.get_template("about.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+# Clients   
+def Clients(request):
+    template = loader.get_template("clients.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+# Blogs
+def Blogs(request):
+    template = loader.get_template("blog.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+
+#  Contact
 def contact(request):
     template = loader.get_template("contact.html")
     
